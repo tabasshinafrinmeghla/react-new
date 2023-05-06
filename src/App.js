67847ba35107130477ec5e43c9ee029e11435453
tabasshinafrinmeghla <tabasshinafrinmeghla@gmail.com>
2023-05-06 13:23:55 +0600

    frame

--- src/App.js
+++ src/App.js
@@ -2,10 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const nayok =['shakib','emon','riyaj','ferdous','ananto','nerob','shuvo'];
   return (
     <div className="App">
-      <Person></Person>
-      <Person></Person>
+      {/* {
+        nayok.map(nayoks => <li>Name: {nayoks}</li>)
+      } */}
+      {
+        nayok.map(nayoks => <Person>Name={nayoks}</Person> )
+      }
+
+      {
+        nayok.map(nayoks => <person name={nayoks}></person>)
+
+      }
+
+      {/* <Person name={nayok[0]} nayka="bangladesh"></Person>
+      <Person name={nayok[1]} nayka="bangladesh"></Person>
+      <Person name={nayok[2]} nayka="bangladesh"></Person>
+      <Person name={nayok[3]} nayka="bangladesh"></Person> */}
       <Person></Person>
       
     </div>
@@ -16,9 +32,10 @@ function Person(props) {
   console.log(props);
   return (
     <div className='person'>
-       <h1>shakib al hasan </h1>
-       <p>profession : Cricketer </p>
-       <p>team: Bangladesh</p>
+       <h1>{props.name}</h1>
+       <p>{props.nayok}</p>
+       <p> </p>
+       {/* <p>team: Bangladesh</p> */}
     </div>
   )
 }
